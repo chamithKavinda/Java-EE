e47a1186a4aebc5a0eb502051877029318cfcc0a
chamithKavinda <chamith13kavinda@gmail.com>
2024-06-30 15:54:52 +0530

    day 1 complete

--- src/main/java/lk/ijse/gdse68/javaeeintro/Demo.java
+++ src/main/java/lk/ijse/gdse68/javaeeintro/Demo.java
@@ -1,12 +1,13 @@
 package lk.ijse.gdse68.javaeeintro;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-
+@WebServlet(urlPatterns = "/demo",loadOnStartup = 2)
 public class Demo extends HttpServlet {
 
     @Override
